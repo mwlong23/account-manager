19f3b83671514e7cf88e919f9a1a356f2da84064
Mitch Long and snow vilay <mitch@>
2017-08-22 16:34:16 -0700

    fix submission bug to take user input and grab value

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,21 @@
 // user interface logic
 $(document).ready(function() {
 
+ $("form#initial-submit").submit(function(event){
+    event.preventDefault();
 
+    var name = $("#name").val();
+    var initialDeposit = $("#initial-deposit").val();
+
+    });
+
+  $("form#add-withdrawl").submit(function(event){
+    event.preventDefault();
+
+
+     var newDeposit = $("#new-deposit").val();
+     var newWithdrawl = $("#new-withdrawl").val();
+
+   });
+   $("")
 });
